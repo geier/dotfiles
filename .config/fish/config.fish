9f5d473d8399b81f010b77a68b27b58bbaf45af1
Christian Geier <geier@lostpackets.de>
2016-05-05 15:49:56 +0200

    fish: tell fish about ~/bin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,6 +16,8 @@ set __fish_git_prompt_color_dirtystate red
 set __fish_git_prompt_char_untrackedfiles '… '
 set __fish_git_prompt_char_cleanstate '✔ '
 
+set PATH ~/bin $PATH
+
 # don't forget to `pip install virtualfish`
 eval (python -m virtualfish)
 
