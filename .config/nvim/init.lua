b3385d0275ab9c34fa6178c9d95324701303a18b
Christian Geier <geier@lostpackets.de>
2022-02-10 00:33:17 +0100

    nvim: boostrap packer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,12 @@
 local fn = vim.fn
 local cmd = vim.cmd
 
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
 require('plugins')
 cmd [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
 cmd [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
