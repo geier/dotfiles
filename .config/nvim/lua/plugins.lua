6145b62dbd7c75d9a55a68836941e7e6fc291f3a
Christian Geier <christian.geier@justadd.ai>
2023-03-15 00:31:17 +0100

    nvim: experiments with yalm companion plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -153,4 +153,17 @@ return require('packer').startup(function()
 
     use 'romgrk/hologram.nvim'
 
+    -- yaml support, for k8s
+    use {
+      "someone-stole-my-name/yaml-companion.nvim",
+      requires = {
+          { "neovim/nvim-lspconfig" },
+          { "nvim-lua/plenary.nvim" },
+          { "nvim-telescope/telescope.nvim" },
+      },
+      config = function()
+        require("telescope").load_extension("yaml_schema")
+      end,
+    }
+
 end)
