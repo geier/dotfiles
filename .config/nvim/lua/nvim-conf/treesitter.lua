179f2b88bda2e2c1b4a82ebdd86a9b21abe3bbfa
Christian Geier <christian.geier@justadd.ai>
2023-05-29 12:48:42 +0200

    nvim: more treesitter

--- .config/nvim/lua/nvim-conf/treesitter.lua
+++ .config/nvim/lua/nvim-conf/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "vimdoc", "javascript", "typescript", "python", "lua", "yaml" },
+  ensure_installed = { "vimdoc", "javascript", "typescript", "python", "lua", "yaml", "org"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -12,12 +12,12 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
+    disable = {'org'},
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
   },
 }
-
